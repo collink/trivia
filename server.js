2baf7b08f55a25eb0d1789c5f79ea60349226cce
Collin Klopfenstein <collin.klopfenstein@gmail.com>
2021-01-17 20:07:18 -0600

    Add custom server.js to allow support for socket.io

---
+++ server.js
@@ -0,0 +1,33 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const next = require('next');
+
+const IS_DEV = process.env.NODE_ENV !== 'production';
+const PORT = IS_DEV ? 3000 : process.env.PORT;
+
+const nextApp = next({ dev: IS_DEV });
+const nextHandler = nextApp.getRequestHandler();
+
+io.on('connect', socket => {
+    setInterval(() => {
+        socket.emit('now', {
+            message: 'hello',
+            timeSent: Date.now()
+        });
+    }, 1000);
+});
+
+nextApp
+    .prepare()
+    .then(() => {
+        app.get('*', (req, res) => nextHandler(req, res));
+
+        server.listen(PORT, err => {
+           if (err) {
+               throw err;
+           }
+
+           console.log(`Listening on port ${PORT}`);
+        });
+    })
